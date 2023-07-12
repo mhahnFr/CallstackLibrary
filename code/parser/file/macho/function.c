fd3103ae17e89d6e7f9a25ffc535390523ad41e8
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-07-12 16:26:22 +0200

    Added function representation structure

---
+++ code/parser/file/macho/function.c
@@ -0,0 +1,46 @@
+/*
+ * Callstack Library - A library creating human readable call stacks.
+ *
+ * Copyright (C) 2023  mhahnFr
+ *
+ * This file is part of the CallstackLibrary. This library is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this library, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <stdlib.h>
+
+#include "function.h"
+
+struct function * function_new(void) {
+    struct function * toReturn = malloc(sizeof(struct function));
+    if (toReturn != NULL) {
+        function_create(toReturn);
+    }
+    return toReturn;
+}
+
+void function_create(struct function * self) {
+    self->startAddress = 0x0;
+    self->linkedName   = NULL;
+    self->next         = NULL;
+}
+
+void function_destroy(struct function * self) {
+    // TODO: Implement
+    (void) self;
+}
+
+void function_delete(struct function * self) {
+    function_destroy(self);
+    free(self);
+}
---
+++ code/parser/file/macho/function.h
@@ -0,0 +1,39 @@
+/*
+ * Callstack Library - A library creating human readable call stacks.
+ *
+ * Copyright (C) 2023  mhahnFr
+ *
+ * This file is part of the CallstackLibrary. This library is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this library, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef function_h
+#define function_h
+
+#include <stdint.h>
+
+struct function {
+    uint64_t startAddress;
+    
+    char * linkedName;
+    
+    struct function * next;
+};
+
+struct function * function_new(void);
+void function_create(struct function * self);
+
+void function_destroy(struct function * self);
+void function_delete(struct function * self);
+
+#endif /* function_h */
