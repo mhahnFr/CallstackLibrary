df7d4bf8b39544fca83fd1faf03b225746e53886
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-10-16 19:04:35 +0200

    Documented the callstack frame

--- include/callstack_frame.h
+++ include/callstack_frame.h
@@ -24,15 +24,28 @@
 
 #include "optional_Dl_info.h"
 
+/**
+ * This structure represents a translated callstack frame.
+ */
 struct callstack_frame {
+    /** The dynamic loader info if available.                */
     optional_Dl_info_t info;
     
+    /** The name of the binary file this frame is in.        */
     char * binaryFile;
+    /** The name of the function this frame is in.           */
     char * function;
+    /** The name of the source file this frame is in.        */
     char * sourceFile;
+    /** The line number in the source file this frame is on. */
     unsigned long sourceLine;
 };
 
+/**
+ * Constructs the given callstack frame.
+ *
+ * @param self the callstack frame to be initialized
+ */
 static inline void callstack_frame_create(struct callstack_frame * self) {
     self->binaryFile = NULL;
     self->function   = NULL;
@@ -42,6 +55,11 @@ static inline void callstack_frame_create(struct callstack_frame * self) {
     self->info.has_value = false;
 }
 
+/**
+ * Allocates a new and initialized callstack frame.
+ *
+ * @return the allocated callstack frame or `NULL` if unable to allocate
+ */
 static inline struct callstack_frame * callstack_frame_new(void) {
     struct callstack_frame * toReturn = (struct callstack_frame *) malloc(sizeof(struct callstack_frame));
     
@@ -52,16 +70,38 @@ static inline struct callstack_frame * callstack_frame_new(void) {
     return toReturn;
 }
 
+/**
+ * Allocates a new callstack frame and deeply copies the given callstack frame.
+ *
+ * @param self the callstack frame to be copied
+ * @return a copy of the given callstack frame or `NULL` if unable to allocate
+ */
 struct callstack_frame * callstack_frame_copy(struct callstack_frame * self);
 
+/**
+ * Copies the given callstack frame into the given destination.
+ *
+ * @param destination the callstack frame filled with the copy
+ * @param source the callstack frame to be copied
+ */
 void callstack_frame_copyHere(struct callstack_frame * destination, const struct callstack_frame * source);
 
+/**
+ * Destructs the given callstack frame.
+ *
+ * @param self the callstack frame to be destructed
+ */
 static inline void callstack_frame_destroy(struct callstack_frame * self) {
     free(self->binaryFile);
     free(self->function);
     free(self->sourceFile);
 }
 
+/**
+ * Destroys and deallocates the given callstack frame.
+ *
+ * @param self the callstack frame to be deleted
+ */
 static inline void callstack_frame_delete(struct callstack_frame * self) {
     callstack_frame_destroy(self);
     free(self);
