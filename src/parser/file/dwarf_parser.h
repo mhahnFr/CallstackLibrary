271b68591142f3b48273c2b6c9595a1d27967bcc
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-01-19 10:07:29 +0100

    Added a dwarf parser header

---
+++ src/parser/file/dwarf_parser.h
@@ -0,0 +1,27 @@
+/*
+ * Callstack Library - Library creating human-readable call stacks.
+ *
+ * Copyright (C) 2024  mhahnFr
+ *
+ * This file is part of the CallstackLibrary. This library is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this library, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef dwarf_parser_h
+#define dwarf_parser_h
+
+typedef void (*)(void) dwarf_line_callback;
+
+bool dwarf_parseLineProgram(void* begin, dwarf_line_callback cb);
+
+#endif /* dwarf_parser_h */
