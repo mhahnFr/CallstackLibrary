1fdb152f80aa89345651ce629c7e10c2303f9049
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-01-19 15:34:45 +0100

    Implemented DWARF version and bitness branching

--- src/parser/file/dwarf_parser.c
+++ src/parser/file/dwarf_parser.c
@@ -17,4 +17,63 @@
  * this library, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include <stddef.h>
+#include <stdint.h>
+
 #include "dwarf_parser.h"
+
+static inline bool dwarf_parseLineProgramV4(void* begin, size_t counter, uint64_t actualSize, dwarf_line_callback cb) {
+    // TODO: Imeplement
+    
+    (void) begin;
+    (void) counter;
+    (void) actualSize;
+    (void) cb;
+    
+    return true;
+}
+
+static inline bool dwarf_parseLineProgramV4_64(void* begin, size_t counter, uint64_t actualSize, dwarf_line_callback cb) {
+    // TODO: Implement
+    
+    (void) begin;
+    (void) counter;
+    (void) actualSize;
+    (void) cb;
+    
+    return true;
+}
+
+bool dwarf_parseLineProgram(void* begin, dwarf_line_callback cb) {
+    size_t counter = 0;
+    
+    const uint32_t size = *((uint32_t*) begin);
+    counter += 4;
+    
+    bool     bit64;
+    uint64_t actualSize;
+    if (size == 0xffffffff) {
+        actualSize = *((uint64_t*) (begin + counter));
+        bit64      = true;
+        counter += 8;
+    } else {
+        actualSize = size;
+        bit64      = false;
+    }
+    const uint16_t version = *((uint16_t*) (begin + counter));
+    counter += 2;
+    
+    if (bit64) {
+        switch (version) {
+            case 4: return dwarf_parseLineProgramV4_64(begin, counter, actualSize, cb);
+                
+            default: return false;
+        }
+    }
+    switch (version) {
+        case 4: return dwarf_parseLineProgramV4(begin, counter, actualSize, cb);
+            
+        default: return false;
+    }
+    return false;
+}
