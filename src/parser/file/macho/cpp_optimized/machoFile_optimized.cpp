d51d6cee3c271f4fa8329140035376b9535eb064
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-01-29 16:07:16 +0100

    Added C++ optimized version of the macho file abstraction

---
+++ src/parser/file/macho/cpp_optimized/machoFile_optimized.cpp
@@ -0,0 +1,117 @@
+/*
+ * Callstack Library - Library creating human-readable call stacks.
+ *
+ * Copyright (C) 2024  mhahnFr
+ *
+ * This file is part of the CallstackLibrary. This library is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this library, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "../machoFile.h"
+
+#include <list>
+#include <map>
+
+class MachoFile {
+    machoFile self;
+    
+    std::list<objectFile*> objectFiles;
+    std::map<uint64_t, std::pair<function, objectFile*>, std::greater<uint64_t>> functions;
+    
+public:
+    inline MachoFile() {
+        machoFile_create(*this);
+        self.priv = this;
+    }
+    
+    inline ~MachoFile() noexcept {
+        machoFile_destroy(&self._);
+        for (auto& file : objectFiles) {
+            objectFile_delete(file);
+        }
+//        for (auto& [address, [function, filePtr]] : functions) { // ?
+//            function_destroy(&function);
+//        }
+    }
+    
+    constexpr inline void addFunction(function&& function) {
+        functions.emplace(std::make_pair(function.startAddress, std::make_pair(function, nullptr)));
+    }
+    
+    constexpr inline void addObjectFile(objectFile* file) {
+        objectFiles.push_front(file);
+        objectFile_functionsForEach(file, [](function* it, va_list args) {
+            MachoFile*  self = reinterpret_cast<MachoFile*> (va_arg(args, void*));
+            objectFile* file = reinterpret_cast<objectFile*>(va_arg(args, void*));
+            
+            self->functions.insert_or_assign(it->startAddress, std::make_pair(*it, file));
+        }, this, file);
+    }
+    
+    inline auto getDebugInfo(void* addr) -> optional_debugInfo_t {
+        const uint64_t address = (reinterpret_cast<uint64_t>(addr) - reinterpret_cast<uint64_t>(self._.startAddress)) + self.addressOffset;
+        auto it = functions.upper_bound(address);
+        if (it == functions.end()) {
+            return { .has_value = false };
+        }
+        optional_debugInfo_t info = { .has_value = false };
+        if (it->second.second != nullptr) {
+            info = objectFile_getDebugInfo(it->second.second, address);
+        }
+        if (!info.has_value) {
+            info = {
+                true, {
+                    it->second.first,
+                    { .has_value = false }
+                }
+            };
+        }
+        return info;
+    }
+    
+    inline operator machoFile*() {
+        return &self;
+    }
+    
+    inline auto operator->() -> machoFile* {
+        return *this;
+    }
+};
+
+auto machoFile_new() -> machoFile* {
+    try {
+        return *(new MachoFile());
+    } catch (...) {
+        return nullptr;
+    }
+}
+
+void machoFile_addFunction(machoFile* me, function function) {
+    reinterpret_cast<MachoFile*>(me->priv)->addFunction(std::move(function));
+}
+
+void machoFile_addObjectFile(machoFile* me, objectFile* file) {
+    reinterpret_cast<MachoFile*>(me->priv)->addObjectFile(file);
+}
+
+auto machoFile_getDebugInfo(machoFile* me, void* address) -> optional_debugInfo_t {
+    return reinterpret_cast<MachoFile*>(me->priv)->getDebugInfo(address);
+}
+
+void machoFile_destroy(binaryFile*) {
+    // Nothing to do so far.    - mhahnFr
+}
+
+void machoFile_delete(binaryFile* me) {
+    delete reinterpret_cast<MachoFile*>(machoFileOrNull(reinterpret_cast<binaryFile*>(me->concrete)));
+}
