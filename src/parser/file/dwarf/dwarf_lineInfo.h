61efd85de7541cf10b03ac24acaf145fc46e562f
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-04-13 11:05:11 +0200

    Added a value destructor for the dwarf line info structure

--- src/parser/file/dwarf/dwarf_lineInfo.h
+++ src/parser/file/dwarf/dwarf_lineInfo.h
@@ -3,18 +3,20 @@
  *
  * Copyright (C) 2024  mhahnFr
  *
- * This file is part of the CallstackLibrary. This library is free software:
- * you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation,
- * either version 3 of the License, or (at your option) any later version.
+ * This file is part of the CallstackLibrary.
  *
- * This library is distributed in the hope that it will be useful,
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The CallstackLibrary is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with
- * this library, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with the
+ * CallstackLibrary, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef dwarfLineInfo_h
@@ -58,13 +60,17 @@ struct dwarf_lineInfo {
     bool epilogueBegin;
 };
 
+static inline void dwarf_lineInfo_destroyValue(struct dwarf_lineInfo self) {
+    free((void*) self.fileName);
+}
+
 /**
  * Destroys the given DWARF line info.
  *
  * @param self the line info to be destroyed
  */
 static inline void dwarf_lineInfo_destroy(struct dwarf_lineInfo* self) {
-    free((void*) self->fileName);
+    dwarf_lineInfo_destroyValue(*self);
 }
 
 #endif /* dwarfLineInfo_h */
