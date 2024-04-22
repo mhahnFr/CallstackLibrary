207ac4e9937259c9e5ca288811f60d9bdf5af17b
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-04-22 17:08:19 +0200

    Added uint64 pair

---
+++ src/parser/file/dwarf/pair_uint64.h
@@ -0,0 +1,29 @@
+/*
+ * Callstack Library - Library creating human-readable call stacks.
+ *
+ * Copyright (C) 2024  mhahnFr
+ *
+ * This file is part of the CallstackLibrary.
+ *
+ * CallstackLibrary is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CallstackLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with the
+ * CallstackLibrary, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef pair_uint64_h
+#define pair_uint64_h
+
+#include "../../../../DC4C/pair.h"
+
+typedef_pair_named(uint64, uint64_t, uint64_t);
+
+#endif /* pair_uint64_h */
