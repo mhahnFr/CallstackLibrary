eb26877c9b06a18f646b5413324fd5963e979f3b
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-04-22 11:21:02 +0200

    Added builtin function abstraction header

---
+++ include/lcs_builtins.h
@@ -0,0 +1,41 @@
+/*
+ * CallstackLibrary - Library creating human-readable call stacks.
+ *
+ * Copyright (C) 2024  mhahnFr
+ *
+ * This file is part of the CallstackLibrary.
+ *
+ * The CallstackLibrary is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The CallstackLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with the
+ * CallstackLibrary, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef lcs_builtins_h
+#define lcs_builtins_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef LCS_USE_BUILTINS
+ #define lcs_returnAddress(level) (__builtin_return_address(level))
+#else
+ #include <stddef.h>
+
+ #define lcs_returnAddress(level) (NULL)
+#endif
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif /* lcs_builtins_h */
