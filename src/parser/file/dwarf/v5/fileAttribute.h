ee4a81a7f28db4b6b83c9a4902c70491b4515ed1
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-04-26 16:12:51 +0200

    Added file attributes structure and vector

---
+++ src/parser/file/dwarf/v5/fileAttribute.h
@@ -0,0 +1,35 @@
+/*
+ * Callstack Library - Library creating human-readable call stacks.
+ *
+ * Copyright (C) 2024  mhahnFr
+ *
+ * This file is part of the CallstackLibrary.
+ *
+ * CallstackLibrary is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CallstackLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with the
+ * CallstackLibrary, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef fileAttribute_h
+#define fileAttribute_h
+
+#include <stdint.h>
+
+struct fileAttribute {
+    char*    path;
+    uint64_t index;
+    uint64_t timestamp;
+    uint64_t size;
+    uint8_t* md5;
+};
+
+#endif /* fileAttribute_h */
