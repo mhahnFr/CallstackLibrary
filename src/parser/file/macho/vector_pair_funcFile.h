bcc72aa873817c5145102064bc7116ae6da2d9c2
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-05-11 11:04:11 +0200

    Adapted the Mach-O parsing part

--- src/parser/file/macho/vector_pair_funcFile.h
+++ src/parser/file/macho/vector_pair_funcFile.h
@@ -1,28 +1,30 @@
 /*
- * Callstack Library - Library creating human-readable call stacks.
+ * CallstackLibrary - Library creating human-readable call stacks.
  *
  * Copyright (C) 2024  mhahnFr
  *
- * This file is part of the CallstackLibrary. This library is free software:
- * you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation,
- * either version 3 of the License, or (at your option) any later version.
+ * This file is part of the CallstackLibrary.
  *
- * This library is distributed in the hope that it will be useful,
+ * The CallstackLibrary is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The CallstackLibrary is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with
- * this library, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with the
+ * CallstackLibrary, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef vector_pair_funcFile_h
 #define vector_pair_funcFile_h
 
-#include "optional_pair_funcFile.h"
+#include <vector.h>
 
-#include "../../../../DC4C/vector.h"
+#include "optional_pair_funcFile.h"
 
 typedef_vector_light_named(pairFuncFile, pair_funcFile_t);
 
