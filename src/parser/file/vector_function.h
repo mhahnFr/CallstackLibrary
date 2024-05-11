3f8cd0e7705ea85f18692a6bacccd48449162f41
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-05-11 10:50:55 +0200

    Adapted the file handling files

--- src/parser/file/vector_function.h
+++ src/parser/file/vector_function.h
@@ -1,28 +1,28 @@
 /*
- * Callstack Library - Library creating human-readable call stacks.
+ * CallstackLibrary - Library creating human-readable call stacks.
  *
  * Copyright (C) 2023 - 2024  mhahnFr
  *
  * This file is part of the CallstackLibrary.
  *
- * The Callstack library is free software: you can redistribute it and/or modify
+ * The CallstackLibrary is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * The Callstack Library is distributed in the hope that it will be useful,
+ * The CallstackLibrary is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along with the
- * Callstack library, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ * CallstackLibrary, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef vector_function_h
 #define vector_function_h
 
-#include "../../../DC4C/vector.h"
+#include <vector.h>
 
 #include "function.h"
 
