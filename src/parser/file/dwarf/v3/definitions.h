eaf7e2871264dcb527676cd946a8dbe55276c638
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-05-21 18:08:29 +0200

    Added DWARF3 definitions

---
+++ src/parser/file/dwarf/v3/definitions.h
@@ -0,0 +1,31 @@
+/*
+ * CallstackLibrary - Library creating human-readable call stacks.
+ *
+ * Copyright (C) 2024  mhahnFr
+ *
+ * This file is part of the CallstackLibrary.
+ *
+ * The CallstackLibrary is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The CallstackLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with the
+ * CallstackLibrary, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef dwarf_v3_definitions_h
+#define dwarf_v3_definitions_h
+
+#include "../definitions.h"
+
+#define DW_LNS_set_prologue_end   0x0a
+#define DW_LNS_set_epilogue_begin 0x0b
+#define DW_LNS_set_isa            0x0c
+
+#endif /* dwarf_v3_definitions_h */
