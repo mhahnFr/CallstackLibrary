61af1098a8be3bff7cd9d3982478d65286a12441
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-05-21 10:53:45 +0200

    Added optional file attribute vector

---
+++ src/parser/file/dwarf/v5/optional_vector_fileAttribute.h
@@ -0,0 +1,31 @@
+/*
+ * Callstack Library - Library creating human-readable call stacks.
+ *
+ * Copyright (C) 2024  mhahnFr
+ *
+ * This file is part of the CallstackLibrary.
+ *
+ * The CallstackLibrary is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The CallstackLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with the
+ * CallstackLibrary, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef optional_vector_fileAttribute_h
+#define optional_vector_fileAttribute_h
+
+#include <optional.h>
+
+#include "vector_fileAttribute.h"
+
+typedef_optional_named(vector_fileAttribute, vector_fileAttribute_t);
+
+#endif /* optional_vector_fileAttribute_h */
