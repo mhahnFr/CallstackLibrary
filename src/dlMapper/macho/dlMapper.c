3ee9656e9898e68e8609e06c7a4736e58d6911af
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-05-31 19:33:23 +0200

    Fully implemented Mach-O version of the dlMapper loading

--- src/dlMapper/macho/dlMapper.c
+++ src/dlMapper/macho/dlMapper.c
@@ -19,9 +19,78 @@
  * CallstackLibrary, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include <string.h>
+#include <mach-o/dyld.h>
+
+#include "pair_address.h"
+
 #include "../dlMapper_platform.h"
 
+#include "../../parser/file/macho/macho_utils.h"
+
+static inline const void* dlMapper_platform_loadMachO64(const struct mach_header_64* header, const bool bytesSwapped) {
+    uint64_t vmsize = 0;
+    struct load_command* lc = (void*) header + sizeof(struct mach_header_64);
+    for (uint32_t i = 0; i < macho_maybeSwap(32, bytesSwapped, header->ncmds); ++i) {
+        switch (macho_maybeSwap(32, bytesSwapped, lc->cmd)) {
+            case LC_SEGMENT_64: {
+                struct segment_command_64* cmd = (void*) lc;
+                if (strcmp(cmd->segname, SEG_TEXT) == 0) {
+                    vmsize = macho_maybeSwap(64, bytesSwapped, cmd->vmsize);
+                }
+                break;
+            }
+        }
+
+        lc = (void*) lc + macho_maybeSwap(32, bytesSwapped, lc->cmdsize);
+    }
+    return (void*) header + vmsize;
+}
+
+static inline const void* dlMapper_platform_loadMachO32(const struct mach_header* header, const bool bytesSwapped) {
+    uint32_t vmsize = 0;
+    struct load_command* lc = (void*) header + sizeof(struct mach_header);
+    for (uint32_t i = 0; i < macho_maybeSwap(32, bytesSwapped, header->ncmds); ++i) {
+        switch (macho_maybeSwap(32, bytesSwapped, lc->cmd)) {
+            case LC_SEGMENT: {
+                struct segment_command* cmd = (void*) lc;
+                if (strcmp(cmd->segname, SEG_TEXT) == 0) {
+                    vmsize = macho_maybeSwap(32, bytesSwapped, cmd->vmsize);
+                }
+                break;
+            }
+        }
+
+        lc = (void*) lc + macho_maybeSwap(32, bytesSwapped, lc->cmdsize);
+    }
+    return (void*) header + vmsize;
+}
+
+static inline pair_address_t dlMapper_platform_loadMachO(const struct mach_header* header) {
+    const void* end = NULL;
+    switch (header->magic) {
+        case MH_MAGIC_64:
+        case MH_CIGAM_64: end = dlMapper_platform_loadMachO64((void*) header, header->magic == MH_CIGAM_64); break;
+
+        case MH_MAGIC:
+        case MH_CIGAM: end = dlMapper_platform_loadMachO32(header, header->magic == MH_CIGAM); break;
+
+        default: abort(); // TODO: Make sure no fat handling is needed
+    }
+
+    return (struct pair_address) { header, end };
+}
+
 bool dlMapper_platform_loadLoadedLibraries(vector_loadedLibInfo_t* libs) {
-    // TODO: Implement
-    return false;
+    const uint32_t count = _dyld_image_count();
+    for (uint32_t i = 0; i < count; ++i) {
+        const intptr_t vmaddr = _dyld_get_image_vmaddr_slide(i);
+        const pair_address_t addresses = dlMapper_platform_loadMachO(_dyld_get_image_header(i));
+        vector_loadedLibInfo_push_back(libs, (struct loadedLibInfo) {
+            addresses.first,
+            addresses.second,
+            strdup(_dyld_get_image_name(i))
+        });
+    }
+    return true;
 }
