901330b31dabc9365e9d84cdce3551f908649cf3
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-06-06 19:28:18 +0200

    Documented the ELF structure API

--- src/parser/file/elf/elfFile.h
+++ src/parser/file/elf/elfFile.h
@@ -37,11 +37,17 @@ extern "C" {
  * This structure represents an ELF binary file.
  */
 struct elfFile {
-    /** The super part of this structure. */
+    /** The super part of this structure.                          */
     struct binaryFile _;
 
-    struct lcs_section debugLine, debugLineStr, debugStr;
+    /** The section corresponding to the .debug_line section.      */
+    struct lcs_section debugLine,
+    /** The section corresponding to the .debug_line_str section.  */
+                       debugLineStr,
+    /** The section corresponding to the .debug_str section.       */
+                       debugStr;
     
+    /** The pointer to the private part of this object.            */
     void* priv;
 };
 
@@ -71,12 +77,53 @@ static inline struct elfFile* elfFileOrNull(struct binaryFile * self) {
     return self->type == ELF_FILE ? (struct elfFile*) self->concrete : NULL;
 }
 
+/**
+ * Returns the optionally available debug information for the given address.
+ *
+ * @param self the elf file structure object
+ * @param address the address to get debug information for
+ * @return the optionally available debug information
+ */
 optional_debugInfo_t elfFile_getDebugInfo(struct elfFile* self, void* address);
+
+/**
+ * Loads the represented ELF file.
+ *
+ * @param self the elf file structure object
+ * @return whether the loading was successful
+ */
 bool elfFile_loadFile(struct elfFile* self);
+
+/**
+ * Parses the given buffer into the given elf file structure object.
+ *
+ * @param self the elf file structure object
+ * @param buffer the buffer to be parsed
+ * @param cb the DWARF line callback function
+ * @param args the payload to pass to the given callback
+ * @return whether the parsing was successful
+ */
 bool elfFile_parseFile(struct elfFile* self, void* buffer, dwarf_line_callback cb, void* args);
+
+/**
+ * @brief Adds the given function to the given elf file structure object.
+ *
+ * If a function with the same address already exists, one of them is discarded.
+ *
+ * @param self the elf file structure object
+ * @param f the function object to be added
+ */
 void elfFile_addFunction(struct elfFile* self, struct function f);
 
-/* Heavily WIP. */
+/**
+ * Loads the debug information available for the given address into the given
+ * callstack frame object.
+ *
+ * @param self the binary file object
+ * @param address the address to get debug information for
+ * @param frame the callstack frame object to store the information in
+ * @return whether the callstack frame object was filled successfully
+ */
 bool elfFile_addr2String(struct binaryFile* self, void* address, struct callstack_frame* frame);
 
 /**
