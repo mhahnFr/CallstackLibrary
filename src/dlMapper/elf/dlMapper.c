bee8cc4812203e881483547bbd695b7137dbe442
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-06-25 17:59:50 +0200

    Implemented very basic loaded library iteration

--- src/dlMapper/elf/dlMapper.c
+++ src/dlMapper/elf/dlMapper.c
@@ -19,10 +19,77 @@
  * CallstackLibrary, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include <limits.h>
+#include <string.h>
+#include <unistd.h>
+
+#define _GNU_SOURCE
+#define __USE_GNU
+#include <link.h>
+#undef __USE_GNU
+#undef _GNU_SOURCE
+
 #include "../dlMapper_platform.h"
 
-bool dlMapper_platform_loadLoadedLibraries(vector_loadedLibInfo_t* libs) {
+#include "../macho/pair_address.h"
+
+#include "../../parser/file/binaryFile.h"
+
+struct dlMapper_platform_data {
+    const void* start;
+    vector_loadedLibInfo_t* libs;
+};
+
+static inline char* dlMapper_platform_loadExecutableName(void) {
+    char* buffer = malloc(PATH_MAX);
+    if (buffer == NULL) {
+        return NULL;
+    }
+    ssize_t count = readlink("/proc/self/exe", buffer, PATH_MAX - 1);
+    if (count == -1) {
+        free(buffer);
+        return NULL;
+    }
+    buffer[count] = '\0';
+    return buffer;
+}
+
+static inline int dlMapper_platform_iterateCallback(struct dl_phdr_info* info, size_t size, void* d) {
+    struct dlMapper_platform_data* data = d;
+
+    const char* fileName = info->dlpi_name;
+    bool empty = *fileName == '\0';
+    if (empty) {
+        char* newFileName = dlMapper_platform_loadExecutableName();
+        if (newFileName != NULL) {
+            fileName = newFileName;
+        } else {
+            empty = false;
+        }
+    }
+  //  pair_address_t addresses = dlMapper_platform_loadELF(...);
+    vector_loadedLibInfo_push_back(data->libs, (struct loadedLibInfo) {
+        NULL,//begin,
+        NULL,//end,
+        empty ? (char*) fileName : strdup(fileName),
+        binaryFile_toAbsolutePath(fileName),
+        binaryFile_toRelativePath(fileName),
+        NULL == data->start
+    });
+    __builtin_printf("%s\n", fileName);
+    return 0;
+}
+
+static inline void* dlMapper_platform_loadLCSAddress(void) {
     // TODO: Implement
-    (void) libs;
-    return false;
+    return NULL;
+}
+
+bool dlMapper_platform_loadLoadedLibraries(vector_loadedLibInfo_t* libs) {
+    struct dlMapper_platform_data data = (struct dlMapper_platform_data) {
+        dlMapper_platform_loadLCSAddress(),
+        libs
+    };
+
+    return dl_iterate_phdr(dlMapper_platform_iterateCallback, &data) == 0;
 }
