38f462cfd4f8bf3563fae12828f6b3972d5b0a11
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-07-13 19:11:15 +0200

    Corrected some copyright notices

--- src/parser/file/dwarf/v5/fileAttribute.h
+++ src/parser/file/dwarf/v5/fileAttribute.h
@@ -1,16 +1,16 @@
 /*
- * Callstack Library - Library creating human-readable call stacks.
+ * CallstackLibrary - Library creating human-readable call stacks.
  *
  * Copyright (C) 2024  mhahnFr
  *
  * This file is part of the CallstackLibrary.
  *
- * CallstackLibrary is free software: you can redistribute it and/or modify
+ * The CallstackLibrary is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * CallstackLibrary is distributed in the hope that it will be useful,
+ * The CallstackLibrary is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
