25484e01fc1b48d22aea9f625ac59481ab552093
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-11-12 17:08:52 +0100

    Adapted the header inclusion definition

--- include/callstack_create.h
+++ include/callstack_create.h
@@ -1,24 +1,26 @@
 /*
  * Callstack Library - Library creating human-readable call stacks.
  *
- * Copyright (C) 2022 - 2023  mhahnFr
+ * Copyright (C) 2022 - 2024  mhahnFr
  *
- * This file is part of the CallstackLibrary. This library is free software:
- * you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation,
- * either version 3 of the License, or (at your option) any later version.
+ * This file is part of the CallstackLibrary.
  *
- * This library is distributed in the hope that it will be useful,
+ * The CallstackLibrary is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The CallstackLibrary is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with
- * this library, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with the
+ * CallstackLibrary, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#ifndef callstack_create_h
-#define callstack_create_h
+#ifndef __lcs_callstack_create_h
+#define __lcs_callstack_create_h
 
 #include "callstack.h"
 
@@ -31,7 +33,7 @@ extern "C" {
  *
  * @param self The callstack object.
  */
-static inline void callstack_create(struct callstack * self) {
+static inline void callstack_create(struct callstack* self) {
     self->backtraceSize     = 0;
     self->frameCount        = 0;
     self->frames            = NULL;
@@ -42,4 +44,4 @@ static inline void callstack_create(struct callstack * self) {
 } // extern "C"
 #endif
 
-#endif /* callstack_create_h */
+#endif /* __lcs_callstack_create_h */
