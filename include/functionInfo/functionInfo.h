d97391cb066e55264648cf1a32308b3025738b40
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2024-11-12 18:05:03 +0100

    Added function info loading API skeleton

---
+++ include/functionInfo/functionInfo.h
@@ -0,0 +1,45 @@
+/*
+ * CallstackLibrary - Library creating human-readable call stacks.
+ *
+ * Copyright (C) 2024  mhahnFr
+ *
+ * This file is part of the CallstackLibrary.
+ *
+ * The CallstackLibrary is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The CallstackLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with the
+ * CallstackLibrary, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef __lcs_functionInfo_functionInfo_h
+#define __lcs_functionInfo_functionInfo_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+struct functionInfo {
+    uintptr_t begin, end;
+};
+
+struct functionInfo functionInfo_loadHint(const char* functionName, const char* libraryName);
+
+static inline struct functionInfo functionInfo_load(const char* functionName) {
+    return functionInfo_loadHint(functionName, NULL);
+}
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif /* __lcs_functionInfo_functionInfo_h */
