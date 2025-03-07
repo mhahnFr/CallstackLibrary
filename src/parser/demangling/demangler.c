7b4d8df46efd3b5c05084335e394314605b34eba
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2025-03-07 10:49:53 +0100

    Added a demangler abstraction

---
+++ src/parser/demangling/demangler.c
@@ -0,0 +1,64 @@
+/*
+ * CallstackLibrary - Library creating human-readable call stacks.
+ *
+ * Copyright (C) 2025  mhahnFr
+ *
+ * This file is part of the CallstackLibrary.
+ *
+ * The CallstackLibrary is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The CallstackLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with the
+ * CallstackLibrary, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <stdbool.h>
+#include <string.h>
+
+#include "demangler.h"
+
+#include "swift/demangler.h"
+
+#ifdef CXX_FUNCTIONS
+# include "cxx/demangler.h"
+#endif
+
+/**
+ * Returns whether the given name is a name mangled in the C++ style.
+ *
+ * @param name the name to be checked
+ * @return whether the name is a mangled one
+ */
+static inline bool callstack_demangle_isCxx(const char* name) {
+    if (strncmp(name, "_Z", 2) == 0 || strncmp(name, "___Z", 4) == 0) {
+        return true;
+    }
+    return strlen(name) >= 11 && strncmp(name, "_GLOBAL_", 8) == 0
+        && (name[8] == '.' || name[8] == '_' || name[8] == '$')
+        && (name[9] == 'D' || name[9] == 'I')
+        && name[10] == '_';
+}
+
+static inline bool callstack_demangle_isSwift(const char* name) {
+    // TODO: Is this correct?
+    return strncmp(name, "$s", 2) == 0;
+}
+
+char* callstack_demangle(char* name) {
+    char* toReturn = name;
+    if (callstack_demangle_isCxx(name)) {
+#ifdef CXX_FUNCTIONS
+        toReturn = callstack_demangle_cxx(name);
+#endif
+    } else if (callstack_demangle_isSwift(name)) {
+        toReturn = callstack_demangle_swift(name);
+    }
+    return toReturn;
+}
--- src/parser/demangling/demangler.h
+++ src/parser/demangling/demangler.h
@@ -1,42 +1,27 @@
 /*
- * Callstack Library - A library creating human readable call stacks.
+ * CallstackLibrary - Library creating human-readable call stacks.
  *
- * Copyright (C) 2022  mhahnFr
+ * Copyright (C) 2025  mhahnFr
  *
- * This file is part of the CallstackLibrary. This library is free software:
- * you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation,
- * either version 3 of the License, or (at your option) any later version.
+ * This file is part of the CallstackLibrary.
  *
- * This library is distributed in the hope that it will be useful,
+ * The CallstackLibrary is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The CallstackLibrary is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with
- * this library, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with the
+ * CallstackLibrary, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#ifndef demangler_hpp
-#define demangler_hpp
+#ifndef callstack_demangling_demangler_h
+#define callstack_demangling_demangler_h
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+char* callstack_demangle(char* name);
 
-/**
- * @brief Demangles the given name.
- *
- * If the given name is demangled, the given string is not freed.
- * If the name has not been demangled, the given string is returned.
- *
- * @param name The name to be demangled.
- * @return The demangled name or the given name, if it could not be demangled.
- */
-char * callstack_demangle(char * name);
-
-#ifdef __cplusplus
-} // extern "C"
-#endif
-
-#endif /* demangler_hpp */
+#endif /* callstack_demangling_demangler_h */
