3e968bf071b8515a0956f6247a928882846df3d4
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2025-03-28 10:49:18 +0100

    Added region information to the binary file abstraction

---
+++ src/parser/file/vector_pair_ptr.h
@@ -0,0 +1,33 @@
+/*
+ * CallstackLibrary - Library creating human-readable call stacks.
+ *
+ * Copyright (C) 2025  mhahnFr
+ *
+ * This file is part of the CallstackLibrary.
+ *
+ * The CallstackLibrary is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The CallstackLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with the
+ * CallstackLibrary, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef vector_pair_ptr_h
+#define vector_pair_ptr_h
+
+#include <stdint.h>
+
+#include <DC4C/pair.h>
+#include <DC4C/vector.h>
+
+typedef_pair_named(ptr, uintptr_t, uintptr_t);
+typedef_vector_light_named(pair_ptr, struct pair_ptr);
+
+#endif /* vector_pair_ptr_h */
