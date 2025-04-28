5bf71ea715917b1f76c00e66d41323ae086c881d
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2025-04-28 18:36:16 +0200

    Added tlv descriptor abstraction

---
+++ src/parser/file/macho/TLVDescriptor.h
@@ -0,0 +1,33 @@
+/*
+ * CallstackLibrary - Library creating human-readable call stacks.
+ *
+ * Copyright (C) 2025  mhahnFr
+ *
+ * This file is part of the CallstackLibrary.
+ *
+ * The CallstackLibrary is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The CallstackLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with the
+ * CallstackLibrary, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef TLVDescriptor_h
+#define TLVDescriptor_h
+
+#include <mach-o/loader.h>
+
+#include <DC4C/v2/vector.h>
+
+typedef struct tlv_descriptor TLVDescriptor;
+
+typedef_vector(TLVDescriptor);
+
+#endif /* TLVDescriptor_h */
