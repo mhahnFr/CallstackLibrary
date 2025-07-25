6f681d809e107c4dfc8bc545edaa28da0d9430f9
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2025-07-25 10:53:50 +0200

    Fixed header inclusions in the ELF file abstraction

--- src/parser/file/elf/elfFile.h
+++ src/parser/file/elf/elfFile.h
@@ -24,10 +24,10 @@
 
 #include "../binaryFile.h"
 #include "../debugInfo.h"
+#include "../function.h"
 #include "../lcs_section.h"
-#include "../vector_function.h"
 
-#include "../dwarf/vector_dwarf_lineInfo.h"
+#include "../dwarf/dwarf_lineInfo.h"
 
 /**
  * This structure represents an ELF binary file.
