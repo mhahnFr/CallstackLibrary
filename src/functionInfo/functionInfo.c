8e2e071ce327bbac494524cec70956f6caabfffd
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2026-01-13 19:51:54 +0100

    Adapted the function info loader accordingly

--- src/functionInfo/functionInfo.c
+++ src/functionInfo/functionInfo.c
@@ -1,7 +1,7 @@
 /*
  * CallstackLibrary - Library creating human-readable call stacks.
  *
- * Copyright (C) 2024 - 2025  mhahnFr
+ * Copyright (C) 2024 - 2026  mhahnFr
  *
  * This file is part of the CallstackLibrary.
  *
@@ -36,26 +36,25 @@
  * @return whether the structure was filled, e.g. whether the function was
  * found in the runtime image
  */
-static inline bool functionInfo_getFrom(struct loadedLibInfo* info,
-                                        const char*           functionName,
-                                        struct functionInfo*  functionInfo) {
-    return functionInfo->found = loadedLibInfo_prepare(info)
-        && binaryFile_getFunctionInfo(info->associated, functionName, functionInfo);
+static inline bool functionInfo_getFrom(struct binaryFile*   info,
+                                        const char*          functionName,
+                                        struct functionInfo* functionInfo) {
+    return functionInfo->found = binaryFile_getFunctionInfo(info, functionName, functionInfo);
 }
 
 struct functionInfo functionInfo_loadHint(const char* functionName, const char* libraryName) {
     struct functionInfo toReturn = (struct functionInfo) { 0, 0, false };
 
     dlMapper_init();
-    if (libraryName != NULL && functionInfo_getFrom(dlMapper_libInfoForFileName(libraryName), functionName, &toReturn)) {
+    if (libraryName != NULL && functionInfo_getFrom(dlMapper_binaryFileForFileName(libraryName), functionName, &toReturn)) {
         if (callstack_autoClearCaches) {
             callstack_clearCaches();
         }
         return toReturn;
     }
 
-    vector_iterate(dlMapper_getLoadedLibraries(), {
-        if (functionInfo_getFrom(element, functionName, &toReturn)) {
+    vector_iterate(dlMapper_getLoadedBinaries(), {
+        if (functionInfo_getFrom(*element, functionName, &toReturn)) {
             break;
         }
     });
