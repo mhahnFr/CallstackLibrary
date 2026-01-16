0ab411680e6e8da8c5311c95fe6319bb5bc2b9df
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2026-01-16 10:18:25 +0100

    Reworked the LCS section abstraction

--- src/parser/file/lcs_section.h
+++ src/parser/file/lcs_section.h
@@ -1,7 +1,7 @@
 /*
  * CallstackLibrary - Library creating human-readable call stacks.
  *
- * Copyright (C) 2024  mhahnFr
+ * Copyright (C) 2024, 2026  mhahnFr
  *
  * This file is part of the CallstackLibrary.
  *
@@ -29,18 +29,11 @@
  */
 struct lcs_section {
     /** Pointer to the actual content. */
-    void* content;
+    const void* content;
     /** The size of the content.       */
     uint64_t size;
 };
 
-/**
- * Initializes the given section structure.
- *
- * @param self the section structure object to be initialized
- */
-static inline void lcs_section_create(struct lcs_section* self) {
-    *self = (struct lcs_section) { NULL, 0 };
-}
+#define lcs_section_initializer (struct lcs_section) { .content = NULL, .size = 0 }
 
 #endif /* lcs_section_h */
