8c994ac5d325455d5d9c006032f09cc2ffc00875
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2026-02-10 10:40:01 +0100

    Documented the new loader function; minor documentation improvements

--- src/parser/file/loader.h
+++ src/parser/file/loader.h
@@ -28,7 +28,8 @@
 /**
  * @brief The simple parser callback function prototype.
  *
- * The first passed parameter is the additional payload, the second one is the loaded file buffer.
+ * The first passed parameter is the additional payload, the second one is the
+ * loaded file buffer.
  *
  * Returns whether the parsing was successful.
  */
@@ -36,16 +37,15 @@ typedef bool (*loader_parser)(void*, void*);
 /**
  * @brief The complex parser callback function prototype.
  *
- * The first passed parameter is the loaded file buffer, the second one the file name, the third one the
- * size of the file and the last parameter is the additional payload.
- *
+ * The first passed parameter is the loaded file buffer, the second one the
+ * file name, the third one the size of the file and the last parameter is the
+ * additional payload.
+ * <br><br>
  * Returns whether the parsing was successful.
  */
 typedef bool (*loader_parserExtended)(void*, const char*, size_t, void*);
 
-/**
- * Union consisting of the two possible parser functions.
- */
+/** Union consisting of the two possible parser functions. */
 union loader_parserFunction {
     /** The simple parser function.  */
     loader_parser parseFunc;
@@ -53,17 +53,37 @@ union loader_parserFunction {
     loader_parserExtended parseFuncExtended;
 };
 
-
+/**
+ * @brief Loads the file of the given name and executes the given parser
+ * function using the payload and the loaded data.
+ *
+ * The function can either be the simple one: It gets the payload as first
+ * argument and the file buffer as second argument. The complex version gets
+ * the file buffer as first argument, the file name as second argument, the
+ * file's size as third argument and the payload as last argument.
+ * <br><br>
+ * The parser function is only called when the file was loaded successfully and
+ * the last modified timestamp of the file either equals the given one or
+ * @c NULL is given as timestamp.
+ *
+ * @param fileName the name of the file to be loaded
+ * @param lastModified
+ * @param func the parser function to be used
+ * @param extended whether to use the complex version of the parser function
+ * @param args the payload passed to the callback
+ * @return whether the file was loaded successfully and the parsing was successful
+ */
 bool loader_loadFileAndExecuteTime(const char* fileName, const time_t* lastModified,
                                    union loader_parserFunction func, bool extended, void* args);
 
 /**
- * @brief Loads the file of the given name and executes the given parser function using the payload and
- * the loaded data.
+ * @brief Loads the file of the given name and executes the given parser
+ * function using the payload and the loaded data.
  *
- * The function can either be the simple one: It gets the payload as first argument and the file buffer as second
- * argument. The complex version gets the file buffer as first argument, the file name as second argument, the file's
- * size as third argument and the payload as last argument.
+ * The function can either be the simple one: It gets the payload as first
+ * argument and the file buffer as second argument. The complex version gets
+ * the file buffer as first argument, the file name as second argument, the
+ * file's size as third argument and the payload as last argument.
  *
  * The parser function is only called when the file was loaded successfully.
  *
