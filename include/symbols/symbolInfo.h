60efea48ade035b2dd6019b8ac7343ea1af18717
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2026-02-13 09:55:13 +0100

    Documented the symbol info public API

--- include/symbols/symbolInfo.h
+++ include/symbols/symbolInfo.h
@@ -30,14 +30,46 @@
 extern "C" {
 #endif
 
+/**
+ * @brief The symbol information structure.
+ *
+ * For the time being, the @c callstack_frame structure is used.
+ *
+ * @since v2.3
+ */
 typedef struct callstack_frame SymbolInfo;
 
+/**
+ * Attempts to deduct the debug information for the symbol represented by the
+ * given address.
+ *
+ * @param address the address to find the closest symbol for
+ * @param info the symbol information structure to be filled, must not be @c NULL
+ * @return whether a symbol was found
+ * @since v2.3
+ */
 bool symbols_getInfo(const void* address, SymbolInfo* info);
 
 #ifdef LCS_USE_UNSAFE_OPTIMIZATION
+/**
+ * @brief Attempts to deduct the debug information for the symbol represented
+ * by the given address.
+ *
+ * The symbol information structure may be filled with cached values.
+ *
+ * @param address the address to find the closest symbol for
+ * @param info the symbol information structure to be filled, must not be @c NULL
+ * @return whether a symbol was found
+ * @since v2.3
+ */
 bool symbols_getInfoCached(const void* address, SymbolInfo* info);
 #endif
 
+/**
+ * Destroys the given symbol information structure.
+ *
+ * @param info the symbol information structure to be destroyed, must not be @c NULL
+ */
 void symbols_destroyInfo(const SymbolInfo* info);
 
 #ifdef __cplusplus
