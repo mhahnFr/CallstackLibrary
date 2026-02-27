f25eca6e64148bf3f3675c04066314279946795a
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2026-02-27 09:57:30 +0100

    Implemented and moved the DC4C failable vector operations exception macros

---
+++ src/parser/file/dc4c_exceptions.h
@@ -0,0 +1,47 @@
+/*
+ * CallstackLibrary - Library creating human-readable call stacks.
+ *
+ * Copyright (C) 2026  mhahnFr
+ *
+ * This file is part of the CallstackLibrary.
+ *
+ * The CallstackLibrary is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The CallstackLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with the
+ * CallstackLibrary, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef CALLSTACKLIBRARY_DC4C_EXCEPTIONS_H
+#define CALLSTACKLIBRARY_DC4C_EXCEPTIONS_H
+
+#include <DC4C/vector.h>
+
+#include "exception.h"
+
+#define vector_reserve_throw(...) do {                                           \
+    if (!vector_reserve(__VA_ARGS__)) {                                          \
+        BFE_THROW_MSG(failedAllocation, "Vector reserve allocation has failed"); \
+    }                                                                            \
+} while (0)
+
+#define vector_push_back_throw(...) do {                                           \
+    if (!vector_push_back(__VA_ARGS__)) {                                          \
+        BFE_THROW_MSG(failedAllocation, "Vector push_back allocation has failed"); \
+    }                                                                              \
+} while (0)
+
+#define vector_insert_throw(...) do {                                           \
+    if (!vector_insert(__VA_ARGS__)) {                                          \
+        BFE_THROW_MSG(failedAllocation, "Vector insert allocation has failed"); \
+    }                                                                           \
+} while (0)
+
+#endif //CALLSTACKLIBRARY_DC4C_EXCEPTIONS_H
